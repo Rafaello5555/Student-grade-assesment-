5562a82b235e301bc4ade8cb2cd7ed90c3a99da7
RAFAIL MAHAMMADLI <75142428+Rafaello5555@users.noreply.github.com>
2020-12-21 21:18:47 +0400

    Add files via upload

---
+++ student_grade_assesment.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int score {};
+char letter_grade {};
+
+int main(){
+  cout << "\n\t\tEnter your score on exam (0-100): ";
+  cin>>score;
+
+  if (score >= 0 && score <= 100){
+      if (score > 90)
+          letter_grade = 'A';
+      else if (score > 80)
+           letter_grade = 'B';
+      else if (score > 70)
+            letter_grade = 'C';
+      else if (score > 60)
+            letter_grade = 'D';
+      else
+            letter_grade = 'F';
+      cout << "\n\t\tYour grade is: " << letter_grade << endl;
+
+      if ( letter_grade == 'F')
+           cout << "\n\t\tSorry, you must repeat the class" << endl << endl;
+      else
+           cout << "\n\t\tCongrats!" << endl << endl;
+  }else{
+      cout << "\n\t\tSorry " << score << " is not in range" << endl << endl;
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
